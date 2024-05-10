8dcf1fd88ba8fdb71a6f7f8577acdcd561aa73a2
Marcin99b <marcin.bigos99@gmail.com>
2024-05-10 21:18:19 +0200

    simple event handling

--- src/Corge/CorgeRunner.cs
+++ src/Corge/CorgeRunner.cs
@@ -5,8 +5,13 @@ public class CorgeRunner
 {
     public void Run()
     {
+        var bus = new EventBus();
+
+        bus.Subscribe<PlayerDecidedEvent>(x => Console.WriteLine(x.Decision));
+        bus.Subscribe<PlayerDecidedEvent>(x => Console.WriteLine(x.Decision));
+
         var player = new Actor("Player", "gold3_1");
-        var dialogHandler = new DialogHandler(player);
+        var dialogHandler = new DialogHandler(bus, player);
         var stefan = new Actor("Stefan", "darkorange3");
 
         dialogHandler.Say(stefan, "Cześć, przyjacielu");
@@ -24,7 +29,7 @@ public class Actor(string name, string color)
     public string Color { get; } = color;
 }
 
-public class DialogHandler(Actor player)
+public class DialogHandler(EventBus bus, Actor player)
 {
     public void Say(Actor actor, string message)
     {
@@ -35,6 +40,44 @@ public class DialogHandler(Actor player)
     {
         var response = AnsiConsole.Prompt(new SelectionPrompt<string>().AddChoices(decisions));
         this.Say(player, response);
+        bus.Publish(new PlayerDecidedEvent(response));
         return response;
+    }
+}
+
+public interface IEvent
+{
+}
+
+public record PlayerDecidedEvent(string Decision) : IEvent;
+
+public class EventBus
+{
+    private readonly Dictionary<Type, List<Action<dynamic>>> subscriptions = new();
+
+    public void Subscribe<T>(Action<T> action) 
+        where T : class, IEvent
+    {
+        var t = typeof(T);
+        Action<dynamic> dyn = x => action(x);
+        if (this.subscriptions.ContainsKey(t))
+        {
+            this.subscriptions[t].Add(dyn);
+        } 
+        else
+        {
+            this.subscriptions.Add(t, [dyn]);
+        }
+    }
+
+    public void Publish(IEvent @event)
+    {
+        if (this.subscriptions.TryGetValue(@event.GetType(), out var list))
+        {
+            foreach (var item in list)
+            {
+                item(@event);
+            }
+        }
     }
 }
