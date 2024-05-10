a46d0c1995b60fc12d63115fc891895512887358
Marcin99b <marcin.bigos99@gmail.com>
2024-05-11 00:55:51 +0200

    simple dialog

--- src/Corge/CorgeRunner.cs
+++ src/Corge/CorgeRunner.cs
@@ -11,8 +11,13 @@ public class CorgeRunner
         var sentence_2 = new Sentence("Myślałem że nie żyjesz");
 
         var option_1 = new DecisionOption("Tak, też tak myślałem");
+        var sentence_3 = new Sentence("Długo tu leżałeś");
+
         var option_2 = new DecisionOption("Kim jesteś?");
+        var sentence_4 = new Sentence("Na razie nie mogę ci powiedzieć");
+
         var option_3 = new DecisionOption("Gdzie ja jestem?");
+        var sentence_5 = new Sentence("Nawet gdybym ci powiedział, to byś nie zrozumiał");
 
         var decision_1 = new Decision(
             [
@@ -25,7 +30,10 @@ public class CorgeRunner
         {
             sentence_1,
             sentence_2,
-            decision_1
+            decision_1,
+            sentence_3,
+            sentence_4,
+            sentence_5,
         };
 
         var stefan = new Actor("Stefan", "darkorange3", sentence_1.Id);
@@ -34,6 +42,14 @@ public class CorgeRunner
         {
             new (stefan.Id, sentence_1.Id, sentence_2.Id),
             new (stefan.Id, sentence_2.Id, decision_1.Id),
+
+            new(stefan.Id, option_1.Id, sentence_3.Id),
+            new(stefan.Id, option_2.Id, sentence_4.Id),
+            new(stefan.Id, option_3.Id, sentence_5.Id),
+
+            new(stefan.Id, sentence_3.Id, decision_1.Id),
+            new(stefan.Id, sentence_4.Id, decision_1.Id),
+            new(stefan.Id, sentence_5.Id, decision_1.Id),
         };
 
         var actors = new Actor[]
@@ -47,6 +63,14 @@ public class CorgeRunner
         {
             var actor = actors.First(actor => actor.Id == x.ActorId);
             var dialogueItem = dialogueItems.First(d => d.Id == actor.StartId);
+            if (dialogueItem is Sentence sentence)
+            {
+                dialogHandler.Say(actor, sentence);
+            }
+            else if (dialogueItem is Decision decision)
+            {
+                dialogHandler.Ask(decision);
+            }
         });
 
         bus.Subscribe<SentenceFinishedEvent>(x => 
@@ -54,6 +78,31 @@ public class CorgeRunner
             var relation = sentenceRelations.First(s => s.FromId == x.SentenceId);
             var actor = actors.First(a => a.Id == relation.ActorId);
             var nextItem = dialogueItems.First(i => i.Id == relation.ContinueId);
+
+            if(nextItem is Sentence sentence)
+            {
+                dialogHandler.Say(actor, sentence);
+            } 
+            else if (nextItem is Decision decision) 
+            {
+                dialogHandler.Ask(decision);
+            }
+        });
+
+        bus.Subscribe<PlayerDecidedEvent>(x => 
+        {
+            var relation = sentenceRelations.First(s => s.FromId == x.OptionId);
+            var actor = actors.First(a => a.Id == relation.ActorId);
+            var nextItem = dialogueItems.First(i => i.Id == relation.ContinueId);
+
+            if (nextItem is Sentence sentence)
+            {
+                dialogHandler.Say(actor, sentence);
+            }
+            else if (nextItem is Decision decision)
+            {
+                dialogHandler.Ask(decision);
+            }
         });
 
         bus.Publish(new PlayerStartedConversationEvent(stefan.Id));
@@ -94,13 +143,15 @@ public class DialogueHandler(EventBus bus)
     public void Say(Actor actor, Sentence sentence)
     {
         AnsiConsole.Markup($"[{actor.Color}]{actor.Name}: [/][grey84]{sentence.Text}[/]\n");
+        bus.Publish(new SentenceFinishedEvent(sentence.Id));
     }
 
     public string Ask(Decision decision)
     {
         var response = AnsiConsole.Prompt(new SelectionPrompt<string>().AddChoices(decision.Options.Select(x => x.Text)));
+        AnsiConsole.Markup($"[gold3_1]Ty: [/][grey84]{response}[/]\n");
         var option = decision.Options.First(x => x.Text == response);
-        bus.Publish(new PlayerDecidedEvent(decision.Id, option.Id));
+        bus.Publish(new PlayerDecidedEvent(option.Id));
         return response;
     }
 }
@@ -110,5 +161,5 @@ public interface IEvent
 }
 
 public record PlayerStartedConversationEvent(Guid ActorId) : IEvent;
-public record PlayerDecidedEvent(Guid DecisionId, Guid OptionId) : IEvent;
+public record PlayerDecidedEvent(Guid OptionId) : IEvent;
 public record SentenceFinishedEvent(Guid SentenceId) : IEvent;
