9473d566bd76f2c9784c7d27adcf5c0693236ff7
Marcin99b <marcin.bigos99@gmail.com>
2024-05-10 21:01:43 +0200

    better console

--- src/Corge/CorgeRunner.cs
+++ src/Corge/CorgeRunner.cs
@@ -1,68 +1,40 @@
-﻿namespace Corge;
+﻿using Spectre.Console;
+
+namespace Corge;
 public class CorgeRunner
 {
     public void Run()
     {
-        var dialogHandler = new DialogHandler();
-        var startActor = new Actor("Stefan");
+        var player = new Actor("Player", "gold3_1");
+        var dialogHandler = new DialogHandler(player);
+        var stefan = new Actor("Stefan", "darkorange3");
 
-        dialogHandler.Say(startActor, "Cześć, przyjacielu");
-        dialogHandler.Say(startActor, "pewnie się zastanawiasz jak się tutaj znalazłeś");
+        dialogHandler.Say(stefan, "Cześć, przyjacielu");
+        dialogHandler.Say(stefan, "pewnie się zastanawiasz, jak się tutaj znalazłeś");
 
-        dialogHandler.ShowDecision(["Kim jesteś?", "Co tutaj robie?", "Cześć"]);
+        dialogHandler.Ask(["Kim jesteś?", "Co tutaj robie?", "Cześć"]);
 
         Console.ReadKey();
     }
 }
 
-public class Actor(string name)
+public class Actor(string name, string color)
 {
     public string Name { get; } = name;
+    public string Color { get; } = color;
 }
 
-public class DialogHandler
+public class DialogHandler(Actor player)
 {
     public void Say(Actor actor, string message)
     {
-        ConsoleWriter.WriteActorLine(actor, message);
+        AnsiConsole.Markup($"[{actor.Color}]{actor.Name}: [/][grey84]{message}[/]\n");
     }
 
-    public void ShowDecision(string[] decisions)
+    public string Ask(string[] decisions)
     {
-        ConsoleWriter.WriteDecisions(decisions);
-    }
-
-    public void OnReply(Actor actor)
-    {
-
-    }
-}
-
-public static class ConsoleWriter
-{
-    public static void WriteActorLine(Actor actor, string message)
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        SlowWrite($"{actor.Name}: ");
-        Console.ForegroundColor = ConsoleColor.DarkYellow;
-        SlowWrite(message + "\n");
-    }
-
-    public static void WriteDecisions(string[] decisions)
-    {
-        Console.ForegroundColor = ConsoleColor.White;
-        for (var i = 0; i < decisions.Length; i++)
-        {
-            SlowWrite($"[{i+1}] {decisions[i]}\n");
-        }
-    }
-
-    private static void SlowWrite(string message)
-    {
-        foreach (var letter in message)
-        {
-            Console.Write(letter);
-            Thread.Sleep(15);
-        }
+        var response = AnsiConsole.Prompt(new SelectionPrompt<string>().AddChoices(decisions));
+        this.Say(player, response);
+        return response;
     }
 }
