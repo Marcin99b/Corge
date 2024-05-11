16b68656fe95555585dd7d6596c7c36156f81a34
Marcin99b <marcin.bigos99@gmail.com>
2024-05-11 03:56:25 +0200

    auto ask last question

--- src/Corge/DialogueHandler.cs
+++ src/Corge/DialogueHandler.cs
@@ -10,16 +10,26 @@ public interface IDialogueHandler
 
 public class DialogueHandler(Storage storage) : IDialogueHandler
 {
-    public void Say(Actor actor, Sentence sentence) => AnsiConsole.Markup($"[{actor.Color}]{actor.Name}: [/][grey84]{sentence.Text}[/]\n");
+    public void Say(Actor actor, Sentence sentence)
+    {
+        AnsiConsole.Markup($"[{actor.Color}]{actor.Name}: [/][grey84]{sentence.Text}[/]\n");
+    }
 
     public DecisionOption Ask(Decision decision)
     {
+        AnsiConsole.WriteLine();
+
         var optionsToShow = decision.Options
             .Where(x => !x.HideAfterUsed || !storage.UsedOptions.Contains(x.Id))
             .Select(x => x.Text);
 
-        var response = AnsiConsole.Prompt(new SelectionPrompt<string>()
-            .AddChoices(optionsToShow));
+        var response = optionsToShow.Count() switch
+        {
+            var x when x > 1 => AnsiConsole.Prompt(new SelectionPrompt<string>().AddChoices(optionsToShow)),
+            1 => optionsToShow.First(),
+            _ => throw new NotImplementedException(),
+        };
+
         AnsiConsole.Markup($"[gold3_1]Ty: [/][grey84]{response}[/]\n");
         var option = decision.Options.First(x => x.Text == response);
         return option;
